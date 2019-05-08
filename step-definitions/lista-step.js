8ecc14676a22ca3cd351d7766bd846212951a16d
Weldisson Araujo <weldissonaraujo@MBPdeWeldisson.lan>
2019-05-08 00:32:09 -0300

    implementando step

--- step-definitions/lista-step.js
+++ step-definitions/lista-step.js
@@ -1,9 +1,22 @@
 const { client } = require('nightwatch-api');
-const { Given, Then } = require('cucumber');
+const { Given, When, Then } = require('cucumber');
 const lista_page = client.page.lista_page();
 
 Given('que eu esteja no site Passei Direto', () => {
     return lista_page
         .navigate()
         .acessarLogin();
+});
+Given('logado no sistema com uma conta gmail', () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+When('salvo um arquivo em uma nova lista', () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('visualizo a nova lista criada com sucesso', () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
 });
